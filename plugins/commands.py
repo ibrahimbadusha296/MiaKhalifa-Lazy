116d43dfbfa2fbed9261a6fa6c321c21ccbbaf99
ibrahimbadusha296 <51526140+ibrahimbadusha296@users.noreply.github.com>
2023-01-28 00:05:01 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -244,7 +244,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-reply_markup=InlineKeyboardMarkup( [[InlineKeyboardButton('Main Channel', url='https://t.me/PrimePornZ')]] ),
+reply_markup=InlineKeyboardMarkup( [ [InlineKeyboardButton('Main Channel', url='https://t.me/PrimePornZ')] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
